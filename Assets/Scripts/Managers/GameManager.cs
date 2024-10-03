afeed42ff728cd5a1492a20a02893eb25ae12eaa
Dimas <dimass.drm@gmail.com>
2024-10-03 19:40:10 +0700

    Add Camera Object to DontDestroyOnLoad

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -19,5 +19,6 @@ public class GameManager : MonoBehaviour
         LevelManager = GetComponentInChildren<LevelManager>();
 
         DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(GameObject.Find("Camera"));
     }
 }
